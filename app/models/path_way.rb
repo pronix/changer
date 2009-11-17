00eac858efed805c7aaaaec7a76385d724a935b5
Maxim <parallel588@gmail.com>
2009-11-17 22:50:42 +0500

    Добавлен поиск способ обмена по валютам

--- app/models/path_way.rb
+++ app/models/path_way.rb
@@ -4,4 +4,19 @@
 class PathWay < ActiveRecord::Base
   belongs_to :currency_source, :class_name => "Currency"
   belongs_to :currency_receiver, :class_name => "Currency" 
+  named_scope :get_currency_for_source, lambda{ |curr| { 
+    :conditions => { :currency_source_id => curr }
+    }}
+  
+  named_scope :get_currency_for_receiver, lambda{ |curr| { 
+      :conditions => { :currency_receiver_id => curr }
+    }}
+  class << self
+    
+    # поиск пути по валютам
+    def find_path(source, receiver)
+      first  :conditions => { :currency_receiver_id => receiver.id, :currency_source_id => source.id }
+    end
+    
+  end
 end
