21913e1d27882f76ff4f48e82c4453bb1b4f9c2e
Maxim <parallel588@gmail.com>
2009-11-17 22:51:08 +0500

    Добавлен вывод сообщение

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def display_flash_message(message = flash)
+    return if message.empty?
+    flash_type = message.keys.first
+    content_tag :div, :id => "flash", :class => "flash_#{flash_type}" do 
+      %{ <p>#{message[flash_type]}</p> }
+    end
+  end
 end
