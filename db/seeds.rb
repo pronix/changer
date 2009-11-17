6859760da1b9cee4ec5f84456720e782863ff83e
Maxim <parallel588@gmail.com>
2009-11-17 13:39:11 +0500

    Данные по плат. системам, валютам для теста

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+
+PaymentSystem.destroy_all
+Currency.destroy_all
+PathWay.destroy_all
+
+PaymentSystem.create([ { 
+                        :name => "Webmoney", 
+                        :description => "Платежная система webmoney"
+                      }, { 
+                         :name => "Яндекс Деньги",
+                         :description => "Яндекс Деньги"
+                      } ] )
+
+webmoney = PaymentSystem.find_by_name "Webmoney"
+yandex_money = PaymentSystem.find_by_name "Яндекс Деньги"
+webmoney.currencies.create([ {
+                               :name => "WMZ", :code => "WMZ",
+                               :description => "$"
+                             }, { 
+                               :name => "WMR", :code => "WMR",
+                               :description => "Руб."
+                             } ])
+yandex_money.currencies.create([
+                                {     
+                                  :name => "Яндекс Деньги",
+                                  :code => "YANDEX_MONEY",
+                                  :description => "YA"
+                                }
+                               ])
+
+wmz = Currency.find_by_code "WMZ"
+wmr = Currency.find_by_code "WMR"
+ya = Currency.find_by_code "YANDEX_MONEY"
+
+PathWay.create([{ 
+                  :currency_source => wmz,
+                  :currency_receiver => ya,
+                  :percent => 1,
+                  :description => "Меняем WMZ на Яндекс Деньги"
+                }, { 
+                  :currency_source => wmr,
+                  :currency_receiver => ya,
+                  :percent => 1,
+                  :description => "Меняем WMR на Яндекс Деньги"
+                }, { 
+                  :currency_source => ya,
+                  :currency_receiver => wmz,
+                  :percent => 1,
+                  :description => "Меняем Яндекс Деньги на WMZ"
+                }, { 
+                  :currency_source => ya,
+                  :currency_receiver => wmr,
+                  :percent => 1,
+                  :description => "Меняем Яндекс Деньги на WMR"
+                } ] )
