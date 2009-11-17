7334b75ebb34b47e2fe93e584c119500f3c2db7d
Maxim <parallel588@gmail.com>
2009-11-17 22:51:56 +0500

    Сделано начальное создание заявки  и отправки на форму дальнейшего заполнения заявки

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -4,9 +4,19 @@ class ClaimsController < ApplicationController
     @claim = Claim.new
   end
 
-  
+  # Создаем новую заявку а обмен
   def create
+    @claim = Claim.new params[:claim]
+    if @claim.save
+      flash[:notice] = 'Создана новая заявка'
+      session[:claim_id] = @claim.id
+      # Отправляем на заполнение параметров куда надо переводить денежные средства
+      redirect_to url_for(:controller => "gateway/#{@claim.currency_receiver.payment_system.controller.downcase}") 
+    else
+
+      flash[:error] = ["<ul>",@claim.errors.full_messages.collect{ |x| "<li>#{x}</li>"}, "</ul>"].join
+      render :action => :index
+    end
   end
-  
 
 end
