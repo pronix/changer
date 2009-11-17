511aa4728033a086b41146ea2ac8d50b7dd9ce4c
Maxim <parallel588@gmail.com>
2009-11-17 13:40:27 +0500

    На главную старницу добавлены формы ввода валют, суммы и кнопка продожить

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,5 +1,12 @@
 class ClaimsController < ApplicationController
+  # Выводим главную страницу на которой есть формы для заполнения заявки
   def index
+    @claim = Claim.new
   end
+
+  
+  def create
+  end
+  
 
 end
