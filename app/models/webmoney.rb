55ebdcdb81b914e90f3e22838b7ad8b27716b016
Maxim <parallel588@gmail.com>
2009-11-19 21:10:19 +0500

    В Webmoney добавлена проверка на валидность кошелька

--- app/models/webmoney.rb
+++ app/models/webmoney.rb
@@ -4,9 +4,17 @@
 
 class Webmoney
   class << self 
+    attr_accessor :errors
+    def errors
+      @@errors rescue { }
+    end
     # валидация параметров option_purse для заявки
     def valid_params(options)
-      true
+      # Проверяем чтоб кошелек был валидны
+      @@errors = { }
+      @@errors[:purse_dest] = "Неверный формат кошелька" unless !options["purse_dest"].blank? &&
+        options["purse_dest"] =~ /^[R|Z][0-9]{12}/
+      @@errors.blank?
     end
     
   end
