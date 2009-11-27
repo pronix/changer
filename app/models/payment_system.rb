972c12063fc86ef862aaad1a81d788e4ccb69018
Maxim <parallel588@gmail.com>
2009-11-27 13:44:13 +0500

    Добавлены параметры для таблицы Валют, для того чтоб некоторые параметры платежных систем можно хранить в таблице валют. Например для плат. системы Webmoney для каждой валюты нужен свой кошелек, и чтоб не делать на каждую валюту свой котроллер, номер кошелька напрмер можно задавать в параметрах валюты.

--- app/models/payment_system.rb
+++ app/models/payment_system.rb
@@ -1,4 +1,11 @@
 class PaymentSystem < ActiveRecord::Base
   has_many :currencies # валюты платежной системы
   serialize :parameters, Hash
+  def parameters(field=nil)
+    if field.blank?
+      read_attribute(:parameters)
+    else
+      read_attribute(:parameters) && read_attribute(:parameters)[field.to_sym]
+    end
+  end
 end
