7a686ed2210e5317fd41d00881149a5a23a5a3b4
Maxim <parallel588@gmail.com>
2009-12-10 00:08:23 +0500

    Добавил вывод резерва и курса для главной страници

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -2,16 +2,21 @@ class ClaimsController < ApplicationController
   # Выводим главную страницу на которой есть формы для заполнения заявки
   def index
     @claim = Claim.new
+    @currency = Currency.be_exchanged
+    @to_currency = PathWay.get_currency_for_source(@currency.first).collect{ |x| [x.currency_receiver.name, x.currency_receiver.id, x.rate] } rescue []
+    @reserv = @currency.first.payment_system.reserve rescue 0
+
   end
   
   # просмотр статус заявки
   def show
-    @claim = Claim.find_claim session[:claim_id] 
-    respond_to do |format|
-      format.html { }
-      format.js { render :partial => 'show', :layout => false,
-        :status => ([:complete,:error, :cancel].include?(@claim.aasm_current_state) ? 404 : :ok) }
-    end
+    
+    # @claim = Claim.find_claim session[:claim_id] 
+    # respond_to do |format|
+    #   format.html { }
+    #   format.js { render :partial => 'show', :layout => false,
+    #     :status => ([:complete,:error, :cancel].include?(@claim.aasm_current_state) ? 404 : :ok) }
+    # end
   end
   
   # Создаем новую заявку а обмен
@@ -26,6 +31,9 @@ class ClaimsController < ApplicationController
       # надо переводить денежные средства
       redirect_to url_for(@claim.filling_action)
     else
+      @currency = Currency.be_exchanged
+      @to_currency = PathWay.get_currency_for_source(@currency.first).collect{ |x| [x.currency_receiver.name, x.currency_receiver.id, x.rate] } rescue []
+      @reserv = @currency.first.payment_system.reserve rescue 0
       flash[:error] = ["<ul>",@claim.errors.full_messages.collect{ |x| "<li>#{x}</li>"}, "</ul>"].join
       render :action => :index
     end
